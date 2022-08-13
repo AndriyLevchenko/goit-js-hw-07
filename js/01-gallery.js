1b944aa9b3232d4b5ec4308fad0992d598311394
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-08-13 20:06:34 +0300

    Домашня робота №7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+console.log (createImageGalleryMarcup (galleryItems));
+
+// 1 Створення та рендер розмітки
+
+function createImageGalleryMarcup (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+        `;
+    }).join('');
+}
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarcup = createImageGalleryMarcup (galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarcup);
+
+// 2 Делегування та відкриття модального вікна по кліку на елементі галереї. 
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick (evt) {
+    evt.preventDefault();
+    const isGalleryEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryEl){
+        return
+    }
+
+    const resourceUrl = evt.target.dataset.source;
+    console.log(resourceUrl);
+
+    const instance = basicLightbox.create(`
+    <img src="${resourceUrl}" >`)
+
+    instance.show()
+
+    const visible = instance.visible()
+    console.log(visible);
+    
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log (createImageGalleryMarcup (galleryItems));
+
+// 1 Створення та рендер розмітки
+
+function createImageGalleryMarcup (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    }).join('');
+}
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarcup = createImageGalleryMarcup (galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarcup);
+
+// 2 Делегування та відкриття модального вікна по кліку на елементі галереї. 
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick (evt) {
+    evt.preventDefault();
+    const isGalleryEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryEl){
+        return
+    }
+
+    const resourceUrl = evt.target.dataset.source;
+    console.log(resourceUrl);
+     
+}
+
+new SimpleLightbox('.gallery a', 
+{captionsData: 'alt', 
+captionPosition: 'bottom',
+fadeSpeed: 250});
+
